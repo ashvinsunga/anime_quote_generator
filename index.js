d15c5f2373e34a0a11f68310b30bcc6c7618f26c
Ashvin <ashvinsunga@yahoo.com.ph>
2021-12-03 22:59:10 +0800

    Added some comments

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ $(document).ready(function () {
   $('.read').on('click', function () {
     var url = 'https://animechan.vercel.app/api/random';
     loading();
-
+    // Fetch JSON data from external API and assign it to a variable
     $.getJSON(url, function (data) {
       var currentQuote = data;
-      //   Check the length of the quote
+      //   Change the class(size) of the text based on the quote's length
       if (currentQuote.quote.length > 150) {
         $('.quoteBody').addClass('longQuote');
       } else if (currentQuote.quote.length > 450) {
@@ -14,11 +14,12 @@ $(document).ready(function () {
         $('.quoteBody').removeClass('longQuote');
         $('.quoteBody').removeClass('longestQuote');
       }
-
+      // Display the data on each HTML Element
       $('.quoteBody').html(currentQuote.quote);
       $('.quoteAnimeCharacter').html(currentQuote.character);
       $('.quoteAnime').html(currentQuote.anime);
       $('.read').html('Next Quote');
+      // Prepare and enable the Tweet button function along with the quotes
       $('.tweetQuote')
         .attr(
           'href',
@@ -32,13 +33,14 @@ $(document).ready(function () {
       complete();
     });
   });
-  //   Show Loading
+  // Assign some HTML elements on variables for Show and Hide Loading functions
   var loader = document.getElementById('loader');
   var quoteBody = document.getElementById('quoteBody');
   var quoteAnimeCharacter = document.getElementById('quoteAnimeCharacter');
   var quoteAnime = document.getElementById('quoteAnime');
   var buttons = document.getElementsByClassName('btn');
 
+  //   Show Loading
   function loading() {
     loader.hidden = false;
     quoteBody.hidden = true;
@@ -47,6 +49,7 @@ $(document).ready(function () {
     buttons[0].hidden = true;
     buttons[1].hidden = true;
   }
+
   //   Hide Loading
   function complete() {
     if (!loader.hidden) {
